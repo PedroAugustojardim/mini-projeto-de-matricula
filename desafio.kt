8d7d3d6f3e85c967d23be9f71ef810529e9998e9
@Pedroaugusto <116207314+PedroAugustojardim@users.noreply.github.com>
2023-09-29 23:43:58 -0300

    Primeira-modificação

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,32 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun Matricular(vararg usuario: Usuario) {
+        for (Usuario in usuario)
+        inscritos.add(Usuario)    
+    }
+    fun ListaInscritos() {
+        for(Inscrito in inscritos){
+        println(Inscrito.nome)
     }
 }
+    fun ListaDeConteudo(){
+        
+        
+    }
 
 fun main() {
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
+    
+    
+ }
